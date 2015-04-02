3ae867ae8dac91e04a0e30557924f67d7cab6be4
cgs5144 <cgs5144@psu.edu>
2015-04-01 22:18:50 -0400

    Update 3/31/2015

--- app/src/main/java/edu/psu/vaultinators/nebulock/ViewVaultEntries.java
+++ app/src/main/java/edu/psu/vaultinators/nebulock/ViewVaultEntries.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class ViewVaultEntry extends Activity {
+public class ViewVaultEntries extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
